3871220f941dd39a821fb60aba8759154f7b3f4f
Valery <Valery@Valery-Asus>
2022-12-30 11:51:41 +0100

    Final commit

--- Reno/src/robot/Robot.java
+++ Reno/src/robot/Robot.java
@@ -16,9 +16,59 @@ public class Robot {
 		this.defensa = generaDefensa();
 	}
 
+	// Creo un constructor copia para mi método pelea
+	public Robot(Robot otroRobot) {
+		this.nombre = otroRobot.nombre;
+		this.puntosVida = otroRobot.puntosVida;
+		this.ataque = otroRobot.ataque;
+		this.defensa = otroRobot.defensa;
+	}
+
+	// Creo el método lucha cuyo parámetro será el otro robot
+	public void lucha(Robot robot2) {
+		Robot robot1 = new Robot(this);
+		String ganador = "";
+		// Creo un bucle para que luchen hasta que se queden sin puntos de vida
+		while (robot1.puntosVida > 0 && robot2.puntosVida > 0) {
+			robot2.puntosVida -= robot1.calcularDanio(robot1.ataque, robot2.defensa);
+			robot2.imprimir();
+			// Creo un if para que sólo sigan luchando si los 2 siguen con vida
+			if (robot2.puntosVida > 0) {
+				robot1.puntosVida -= robot2.calcularDanio(robot2.ataque, robot1.defensa);
+				robot1.imprimir();
+				// para que se generen ataque y defensa nuevos en cada turno
+				robot1.ataque = robot1.generaAtaque();
+				robot1.defensa = robot1.generaDefensa();
+				robot2.ataque = robot2.generaAtaque();
+				robot2.defensa = robot2.generaDefensa();
+			}
+		}
+		if (robot1.puntosVida > 0) {
+			ganador = "Ha ganado " + robot1.nombre;
+		} else {
+			ganador = "Ha ganado " + robot2.nombre;
+		}
+		System.out.println(ganador);
+	}
+
+	// Creo un método para calcular el daño
+	private int calcularDanio(int ataque, int defensa) {
+		int danio = 0;
+		if (ataque > defensa) {
+			danio = ataque;
+		}
+		return danio;
+	}
+
+	// Creo un método para imprimir por pantalla
+	public void imprimir() {
+		System.out.println(
+				"nombre= " + nombre + ", puntosVida=" + puntosVida + ", ataque=" + ataque + ", defensa=" + defensa);
+	}
+
 	// Creo el método para atacar entre 0 y 20 aleatoriamente
 	private int generaAtaque() {
-		int atacar = (int) (Math.random() * 21);
+		int atacar = (int) (Math.random() * 101);
 		return atacar;
 	}
 
@@ -45,9 +95,9 @@ public class Robot {
 		return puntosVida;
 	}
 
-	// Como el ataque sólo puede ser de 0 a 20 lo aseguro
+	// Como el ataque sólo puede ser de 0 a 100 lo aseguro
 	public void setAtaque(int ataque) {
-		if (ataque >= 0 & ataque <= 20) {
+		if (ataque >= 0 & ataque <= 100) {
 			this.ataque = ataque;
 		}
 	}
--- Reno/src/robot/Start.java
+++ Reno/src/robot/Start.java
@@ -3,13 +3,11 @@ package robot;
 public class Start {
 
 	public static void main(String[] args) {
-		//Creo los robots parametrizados y los imprimo.
-				Robot kantam = new Robot("Kantam");
-				System.out.println(kantam);
-				
-				Robot claptrap = new Robot("Claptrap");
-				System.out.println(claptrap);
+		// Creo los robots parametrizados y los imprimo.
+		Robot kantam = new Robot("Kantam");
+		Robot claptrap = new Robot("Claptrap");
 
+		kantam.lucha(claptrap);
 
 	}
 
